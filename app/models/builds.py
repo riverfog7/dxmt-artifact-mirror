8ca38ad8f5041123ab17ea50c8ad7dcc1d48313f
riverfog7 <cjw9770@gmail.com>
2025-12-14 22:48:52 +0900

    update db schema

--- app/models/builds.py
+++ app/models/builds.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import List
 
 from sqlmodel import Field, Relationship, SQLModel
 from pydantic import field_validator
@@ -8,23 +8,51 @@ from pydantic import field_validator
 class BuiltinBuild(SQLModel, table=True):
     __tablename__ = "builtinbuild"
 
-    id: int
+    id: int = Field(primary_key=True)
     commit_sha: str = Field(index=True)
     description: str
     created_at: datetime
     artifact_count: int
 
-    artifacts: List["Artifact"] = Relationship(back_populates="build")
+    artifacts: List["BuiltinArtifact"] = Relationship(back_populates="build")
 
 
-class Artifact(SQLModel, table=True):
-    __tablename__ = "artifact"
+class ReleaseBuild(SQLModel, table=True):
+    __tablename__ = "releasebuild"
+    tag: str = Field(primary_key=True, index=True)
+    created_at: datetime
+    artifact_count: int
 
-    id: int
-    build_id: Optional[int] = Field(default=None, foreign_key="builtinbuild.id", index=True)
+    artifacts: List["ReleaseArtifact"] = Relationship(back_populates="build")
+
+
+class BuiltinArtifact(SQLModel, table=True):
+    # This represents a file artifact produced by a built-in build.
+    # one build can have multiple files (dll, so, etc)
+    __tablename__ = "builtinartifact"
+
+    artifact_id: int = Field(index=True)  # corresponds to GitHub artifact ID can be non-unique
+    build_id: int = Field(foreign_key="builtinbuild.id", index=True)
     name: str  # file name without any path components
 
-    build: Optional["BuiltinBuild"] = Relationship(back_populates="artifacts")
+    build: BuiltinBuild = Relationship(back_populates="artifacts")
+
+    @field_validator("name")
+    def validate_name(cls, v):
+        if "/" in v or "\\" in v:
+            raise ValueError("Artifact name must not contain path components")
+        return v
+
+
+class ReleaseArtifact(SQLModel, table=True):
+    # This represents a file artifact produced by a built-in build.
+    # one build can have multiple files (dll, so, etc)
+    __tablename__ = "releaseartifact"
+    # does not have ID
+    build_tag: str = Field(foreign_key="releasebuild.tag", index=True)
+    name: str  # file name without any path components
+
+    build: ReleaseBuild = Relationship(back_populates="artifacts")
 
     @field_validator("name")
     def validate_name(cls, v):
