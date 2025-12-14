e4e06ad0bff69a9134cc0e5e185e32a7f8be4e21
riverfog7 <cjw9770@gmail.com>
2025-12-14 22:52:30 +0900

    add pkey

--- app/models/builds.py
+++ app/models/builds.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from sqlmodel import Field, Relationship, SQLModel
 from pydantic import field_validator
@@ -8,7 +8,7 @@ from pydantic import field_validator
 class BuiltinBuild(SQLModel, table=True):
     __tablename__ = "builtinbuild"
 
-    id: int = Field(primary_key=True)
+    id: int = Field(primary_key=True)  # the github action run ID
     commit_sha: str = Field(index=True)
     description: str
     created_at: datetime
@@ -19,7 +19,7 @@ class BuiltinBuild(SQLModel, table=True):
 
 class ReleaseBuild(SQLModel, table=True):
     __tablename__ = "releasebuild"
-    tag: str = Field(primary_key=True, index=True)
+    tag: str = Field(primary_key=True, index=True)  # the release tag
     created_at: datetime
     artifact_count: int
 
@@ -31,6 +31,7 @@ class BuiltinArtifact(SQLModel, table=True):
     # one build can have multiple files (dll, so, etc)
     __tablename__ = "builtinartifact"
 
+    id: Optional[int] = Field(default=None, primary_key=True)
     artifact_id: int = Field(index=True)  # corresponds to GitHub artifact ID can be non-unique
     build_id: int = Field(foreign_key="builtinbuild.id", index=True)
     name: str  # file name without any path components
@@ -48,7 +49,7 @@ class ReleaseArtifact(SQLModel, table=True):
     # This represents a file artifact produced by a built-in build.
     # one build can have multiple files (dll, so, etc)
     __tablename__ = "releaseartifact"
-    # does not have ID
+    id: Optional[int] = Field(default=None, primary_key=True)
     build_tag: str = Field(foreign_key="releasebuild.tag", index=True)
     name: str  # file name without any path components
 
